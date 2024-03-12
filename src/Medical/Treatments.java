b2f73cba5905fde5e8209fd7542a706efc34d221
matteov021 <95315996+matteov021@users.noreply.github.com>
2024-03-12 15:11:05 -0600

    Updated Treatments

--- src/Medical/Treatments.java
+++ src/Medical/Treatments.java
@@ -68,7 +68,7 @@ public class Treatments {
 
     /**
      * startHour Setter
-     * @param newStartHour -> an integer
+     * @param newStartHour
      */
 
     public void setStartHour(int startHour) throws IllegalArgumentException {
